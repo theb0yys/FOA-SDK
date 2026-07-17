19a90afaf73ca76018f6819c3603ca6d15a72ada
theb0yys <kanejames103@gmail.com>
2026-07-17 17:52:03 +0100

    Register governance hardening sources

--- Gems/TaintedGrailModdingSDK/Code/taintedgrailmoddingsdk_editor_files.cmake
+++ Gems/TaintedGrailModdingSDK/Code/taintedgrailmoddingsdk_editor_files.cmake
@@ -14,12 +14,16 @@ set(FILES
     Source/CatalogGovernanceBlockerService.h
     Source/CatalogGovernanceService.cpp
     Source/CatalogGovernanceService.h
+    Source/CatalogGovernanceTypes.cpp
+    Source/CatalogGovernanceTypes.h
     Source/CatalogGovernanceWidget.cpp
     Source/CatalogGovernanceWidget.h
     Source/CatalogPersistenceService.cpp
     Source/CatalogPersistenceService.h
     Source/CatalogPromotionService.cpp
     Source/CatalogPromotionService.h
+    Source/CatalogTransactionService.cpp
+    Source/CatalogTransactionService.h
     Source/EconomyAuthoringService.cpp
     Source/EconomyAuthoringService.h
     Source/EconomyBlockerService.cpp

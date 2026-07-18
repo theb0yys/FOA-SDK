4aa3b8806d02e253658b154291f81af103c0984a
theb0yys <kanejames103@gmail.com>
2026-07-18 20:18:49 +0100

    Add Tainted Core source ownership
    Signed-off-by: theb0yys <kanejames103@gmail.com>

---
+++ Gems/TaintedGrailModdingSDK/Code/taintedgrailmoddingsdk_core_files.cmake
@@ -0,0 +1,33 @@
+#
+# Copyright (c) Contributors to the Open 3D Engine Project.
+# For complete copyright and license terms please see the LICENSE at the root of this distribution.
+#
+# SPDX-License-Identifier: Apache-2.0 OR MIT
+#
+
+set(FILES
+    Source/CatalogDatabase.cpp
+    Source/CatalogDatabase.h
+    Source/CatalogGovernanceBlockerService.cpp
+    Source/CatalogGovernanceBlockerService.h
+    Source/CatalogGovernanceService.cpp
+    Source/CatalogGovernanceService.h
+    Source/CatalogGovernanceTypes.cpp
+    Source/CatalogGovernanceTypes.h
+    Source/CatalogPromotionService.cpp
+    Source/CatalogPromotionService.h
+    Source/CatalogTransactionService.cpp
+    Source/CatalogTransactionService.h
+    Source/EconomyAuthoringService.cpp
+    Source/EconomyAuthoringService.h
+    Source/EconomyBlockerService.cpp
+    Source/EconomyBlockerService.h
+    Source/EconomyModels.cpp
+    Source/EconomyModels.h
+    Source/FoundationModels.cpp
+    Source/FoundationModels.h
+    Source/FoundationValidationService.cpp
+    Source/FoundationValidationService.h
+    Source/SourceEvidenceRegistry.cpp
+    Source/SourceEvidenceRegistry.h
+)

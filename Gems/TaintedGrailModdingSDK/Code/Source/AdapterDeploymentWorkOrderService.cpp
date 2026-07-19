70013cf20781b0a984f70fb7da0f23aad3b5d7ea
theb0yys <kanejames103@gmail.com>
2026-07-19 14:22:42 +0100

    Compile deployment work-order service
    Signed-off-by: theb0yys <kanejames103@gmail.com>

---
+++ Gems/TaintedGrailModdingSDK/Code/Source/AdapterDeploymentWorkOrderService.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Contributors to the Open 3D Engine Project.
+ * For complete copyright and license terms please see the LICENSE at the root of this distribution.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR MIT
+ */
+
+#include "AdapterDeploymentWorkOrderService.h"
+
+#include <AzCore/std/algorithm.h>
+#include <AzCore/std/utility/move.h>
+
+#include <cstddef>
+#include <initializer_list>
+#include <locale>
+#include <sstream>
+
+#include "AdapterDeploymentWorkOrderServicePart1.inl"
+#include "AdapterDeploymentWorkOrderServicePart2.inl"
+#include "AdapterDeploymentWorkOrderServicePart3.inl"

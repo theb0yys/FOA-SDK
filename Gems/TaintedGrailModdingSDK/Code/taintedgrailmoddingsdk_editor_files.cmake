9034a4575557af4c2e9873254c377a6c20d1891c
theb0yys <kanejames103@gmail.com>
2026-08-31 03:23:22 +0100

    Compile shared item visual refresh service

--- Gems/TaintedGrailModdingSDK/Code/taintedgrailmoddingsdk_editor_files.cmake
+++ Gems/TaintedGrailModdingSDK/Code/taintedgrailmoddingsdk_editor_files.cmake
@@ -42,6 +42,8 @@ set(FILES
     Source/AdapterVerifierEvidenceReconciliationWidget.h
     Source/AdapterWorkOrderPlanWidget.cpp
     Source/AdapterWorkOrderPlanWidget.h
+    Source/AssetBrowserPreviewRefreshService.cpp
+    Source/AssetBrowserPreviewRefreshService.h
     Source/AssetBrowserPreviewWidget.cpp
     Source/AssetBrowserPreviewWidget.h
     Source/CatalogBrowserWidget.cpp
